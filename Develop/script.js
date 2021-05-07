a14c8f3c280f3ed4bec06be8292b16ec274aaa08
Ursula Wilborn <wilbornu@yahoo.com>
2021-05-07 09:58:30 -0400

    Rev 1- initial code pass

--- Develop/script.js
+++ Develop/script.js
@@ -6,17 +6,39 @@ const loweralpha = "abcdefghijklmnopqrstuvwxyz";
 const upperalpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*+=?/";
-
+var results = '';
 
 
 // Write password to the #password input
 
 function generatePassword() {
   var passwordLength = prompt('How long do you want the password?')
-  if(passwordLength < 8 || passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128) {
     alert('Make sure your password is between 8 and 128 characters!')
     generatePassword()
   }
+  var passwordLowercase = confirm('Do you want lowercase characters?')
+  if (passwordLowercase) {
+    results = results + loweralpha
+  }
+  var passwordUppercase = confirm('Do you want uppercase characters?')
+  if (passwordUppercase) {
+    results = results + upperalpha
+  }
+  var passwordNumbers = confirm('Do you want numbers?')
+  if (passwordNumbers) {
+    results = results + numbers
+  }
+  var passwordSymbols = confirm('Do you want symbols?')
+  if (passwordSymbols) {
+    results = results + symbols
+  }
+  var password = '';
+  for(var i = 1; i <= passwordLength; i++){
+    var randomNumber = Math.floor(Math.random() * results.length);
+    password = password + results[randomNumber]
+  }
+  return password;
 }
 
 
