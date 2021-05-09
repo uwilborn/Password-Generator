1c66d041e724d01b7306c4d806a55bdabcb8301f
Ursula Wilborn <wilbornu@yahoo.com>
2021-05-08 22:55:50 -0400

    fixed code errors in js

--- Develop/script.js
+++ Develop/script.js
@@ -13,38 +13,41 @@ var results = ''; //Created variable to store character types selected
 // Use the prompt feature to prompt the employee for each character preference
 
 function generatePassword() {
-  const passwordLength = prompt('How long do you want the password?')
+  var passwordLength = prompt('How long do you want the password?')
   if (passwordLength < 8 || passwordLength > 128) {
     alert('Make sure your password is between 8 and 128 characters!') // Alerts employee of required length
     generatePassword() // Restarts function if length is not within parameters
   }
-  const passwordLowercase = confirm('Do you want lowercase characters?')
+  var passwordLowercase = confirm('Do you want lowercase characters?')
   if (passwordLowercase) {
     results = results + loweralpha
   }
-  const passwordUppercase = confirm('Do you want uppercase characters?')
+  var passwordUppercase = confirm('Do you want uppercase characters?')
   if (passwordUppercase) {
     results = results + upperalpha
   }
-  const passwordNumbers = confirm('Do you want numbers?')
+  var passwordNumbers = confirm('Do you want numbers?')
   if (passwordNumbers) {
     results = results + numbers
   }
-  const passwordSymbols = confirm('Do you want symbols?')
+  var passwordSymbols = confirm('Do you want symbols?')
   if (passwordSymbols) {
     results = results + symbols
   }
-  else {
-    alert('Please select atleast one character type!') //Alerts employee if no character is selected
-    generatePassword() // Restarts function if no characters are selected
-  }
-  const password = ''; //Create constant to store password iteration
+  // else {
+  //   alert('Please select atleast one character type!') //Alerts employee if no character is selected
+  //   generatePassword() // Restarts function if no characters are selected
+  // }
+  var password = ''; 
   for (var i = 1; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * results.length); //Randomizer function
+    var randomNumber = Math.floor(Math.random() * results.length); 
     password = password + results[randomNumber]
   }
   return password;
 }
+
+console.log(password);
+
 
 
 function writePassword() {
