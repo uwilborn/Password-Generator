c6d9cfa6d86285c58e5f80a623dabbb1ca84f122
Ursula Wilborn <wilbornu@yahoo.com>
2021-05-08 21:48:36 -0400

    Updated with comments, alert codes, and cleaned up the some of the js code

--- Develop/script.js
+++ Develop/script.js
@@ -6,36 +6,41 @@ const loweralpha = "abcdefghijklmnopqrstuvwxyz";
 const upperalpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*+=?/";
-var results = '';
+var results = ''; //Create variable to store character types selected
 
 
 // Write password to the #password input
+// Use the prompt feature to prompt the employee for each character preference
 
 function generatePassword() {
-  var passwordLength = prompt('How long do you want the password?')
+  const passwordLength = prompt('How long do you want the password?')
   if (passwordLength < 8 || passwordLength > 128) {
-    alert('Make sure your password is between 8 and 128 characters!')
-    generatePassword()
+    alert('Make sure your password is between 8 and 128 characters!') // Alerts employee of required length
+    generatePassword() // Restarts function if length is not within parameters
   }
-  var passwordLowercase = confirm('Do you want lowercase characters?')
+  const passwordLowercase = confirm('Do you want lowercase characters?')
   if (passwordLowercase) {
     results = results + loweralpha
   }
-  var passwordUppercase = confirm('Do you want uppercase characters?')
+  const passwordUppercase = confirm('Do you want uppercase characters?')
   if (passwordUppercase) {
     results = results + upperalpha
   }
-  var passwordNumbers = confirm('Do you want numbers?')
+  const passwordNumbers = confirm('Do you want numbers?')
   if (passwordNumbers) {
     results = results + numbers
   }
-  var passwordSymbols = confirm('Do you want symbols?')
+  const passwordSymbols = confirm('Do you want symbols?')
   if (passwordSymbols) {
     results = results + symbols
   }
-  var password = '';
-  for(var i = 1; i <= passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * results.length);
+  else {
+    alert('Please select atleast one character type!') //Alerts employee if no character is selected
+    generatePassword() // Restarts function if no characters are selected
+  }
+  const password = ''; //Create constant to store password iteration
+  for (var i = 1; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * results.length); //Randomizer function
     password = password + results[randomNumber]
   }
   return password;
